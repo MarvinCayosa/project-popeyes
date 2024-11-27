130c996aa4eb87bbaea8053c3f9dba9cc1ae7fe5
liyalliya <virtucio.alliyabernadette@ue.edu.ph>
2024-11-27 13:18:33 +0800

    status section responsiveness

--- php/home.php
+++ php/home.php
@@ -23,6 +23,8 @@
     />
     <link rel="stylesheet" href="../css/home_styles.css" />
     <link rel="stylesheet" href="../css/scrollbar.css" />
+    <link rel="stylesheet" href="../css/faculty_sidebar.css" />
+    <script src="../js/side_bar_script.js" defer></script>
 </head>
 <body>
 
@@ -31,105 +33,7 @@
 
 <div class="frame_1">
     <div class="d-flex">
-        <div
-            class="sidebar_custom sidebar d-flex flex-column flex-shrink-0 p-3 fixed-top"
-            style="width: 110px; height: 100vh"
-        >
-            <!--User Icon Dropdown-->
-            <a
-                href="#"
-                class="user_profile m-2 d-flex justify-content-center text-white text-decoration-none"
-                role="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-            >
-                <span class="icon_name">Marvin Cayosa</span>
-                <span class="icon_name identifier">Administrator</span>
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    class="avatar bi bi-person-circle"
-                    fill="currentColor"
-                    viewBox="0 0 16 16"
-                >
-                    <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
-                    <path
-                        fill-rule="evenodd"
-                        d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"
-                    />
-                </svg>
-            </a>
-            <ul class="user_dropdown dropdown-menu">
-                <li><a class="dropdown-item" href="#">View Profile</a></li>
-                <li><a class="dropdown-item" href="../index.php">Log Out</a></li>
-            </ul>
-
-            <hr />
-            <!--Navigation Icons-->
-            <ul class="nav nav-pills flex-column d-flex align-items-center">
-                <li
-                    class="nav-item nav-link active d-flex justify-content-center icon-container"
-                >
-                    <a href="home.php">
-                        <svg class="icon" fill="currentColor">
-                            <use xlink:href="icons.svg#icon-home"></use>
-                        </svg>
-                    </a>
-                    <span class="icon_name">Home</span>
-                </li>
-                <li class="nav-item d-flex justify-content-center icon-container">
-                    <a href="inbox.php">
-                        <svg class="icon" fill="currentColor">
-                            <use xlink:href="icons.svg#icon-inbox"></use>
-                        </svg>
-                    </a>
-                    <span
-                        class="position-absolute translate-middle badge rounded-pill bg-danger"
-                    >10</span
-                    >
-                    <span class="icon_name">Inbox</span>
-                </li>
-
-                <li class="nav-item d-flex justify-content-center icon-container">
-                    <a href="records.php">
-                        <svg class="icon" fill="currentColor">
-                            <use xlink:href="icons.svg#icon-profile"></use>
-                        </svg>
-                    </a>
-                    <span class="icon_name">Records</span>
-                </li>
-                <li class="nav-item d-flex justify-content-center icon-container">
-                    <span class="icon_name">Archive</span>
-                    <svg class="icon" fill="currentColor">
-                        <use xlink:href="icons.svg#icon-archive"></use>
-                    </svg>
-                </li>
-                <!-- Expand/Collapse Button -->
-                <li class="nav-item d-flex justify-content-center icon-container">
-                    <button class="btn" id="toggleSidebar">
-                        <svg class="icon" fill="currentColor">
-                            <use
-                                xlink:href="icons.svg#icon-right"
-                                id="sidebarToggleIcon"
-                            ></use>
-                        </svg>
-                    </button>
-                </li>
-            </ul>
-
-            <hr />
-
-            <!--Options Icon-->
-            <a
-                href="#"
-                class="options m-2 d-flex justify-content-center text-white text-decoration-none"
-                aria-expanded="false"
-            >
-                <span class="icon_name">Options</span>
-                <svg class="icon" fill="currentColor">
-                    <use xlink:href="icons.svg#icon-menu"></use>
-                </svg>
-            </a>
-        </div>
+        <?php include 'faculty_sidebar.php'; ?>
 
         <div class="main_body flex-grow-1">
             <nav class="navbar-main navbar">
@@ -308,97 +212,100 @@
 
                     <!-- Request Status Section -->
                     <div class="rectangle request-status">
-                        <p style="margin-left: 5px; font-size: 14px; font-weight: 700; font-family: 'Poppins', sans-serif;">
-                            Request Inbox
-                        </p>
-                        <div class="request quick-access-circle"></div>
-                        <div class="separator-line"></div>
-
-                        <!-- Request Entries -->
-                        <div class="student-container">
-                            <div class="student_pic"></div>
-                            <div class="vertical-layout">
-                                <p class="student-name">Alliya Virtucio</p>
-                                <p class="request-date">Nov. 19, 2024</p>
-                            </div>
+                        <div class="status-label-container">
+                            <p class="status-label">Request Inbox</p>
+                            <div class="quick-access-circle request"></div>
                         </div>
 
-                        <div class="student-container">
-                            <div class="student_pic"></div>
-                            <div class="vertical-layout">
-                                <p class="student-name">Marvin Cayosa</p>
-                                <p class="request-date">Nov. 21, 2024</p>
+                        <div class="request-student-container-list">
+                            <!-- Request Entries -->
+                            <div class="student-container">
+                                <div class="student_pic"></div>
+                                <div class="vertical-layout">
+                                    <p class="student-name">Alliya Virtucio</p>
+                                    <p class="request-date">Nov. 19, 2024</p>
+                                </div>
                             </div>
-                        </div>
 
-                        <div class="student-container">
-                            <div class="student_pic"></div>
-                            <div class="vertical-layout">
-                                <p class="student-name">Clarizza Reyes</p>
-                                <p class="request-date">Nov. 30, 2024</p>
+                            <div class="student-container">
+                                <div class="student_pic"></div>
+                                <div class="vertical-layout">
+                                    <p class="student-name">Marvin Cayosa</p>
+                                    <p class="request-date">Nov. 21, 2024</p>
+                                </div>
                             </div>
-                        </div>
 
-                        <div class="student-container">
-                            <div class="student_pic"></div>
-                            <div class="vertical-layout">
-                                <p class="student-name">Vincent Vinas</p>
-                                <p class="request-date">Dec. 21, 2024</p>
+                            <div class="student-container">
+                                <div class="student_pic"></div>
+                                <div class="vertical-layout">
+                                    <p class="student-name">Clarizza Reyes</p>
+                                    <p class="request-date">Nov. 30, 2024</p>
+                                </div>
                             </div>
-                        </div>
 
-                        <!-- See More Link -->
-                        <div class="see-more-container">
-                            <a href="#" class="see-more-link" onclick="toggleRequestStatus()">
-                                See more
-                            </a>
+                            <div class="student-container">
+                                <div class="student_pic"></div>
+                                <div class="vertical-layout">
+                                    <p class="student-name">Vincent Vinas</p>
+                                    <p class="request-date">Dec. 21, 2024</p>
+                                </div>
+                            </div>
+
+                            <!-- See More Link -->
+                            <div class="see-more-container">
+                                <a href="#" class="see-more-link" onclick="toggleRequestStatus()">
+                                    See more
+                                </a>
+                            </div>
                         </div>
                     </div>
 
                     <!-- Return Status Section -->
                     <div class="rectangle return-status">
-                        <p style="margin-left: 5px; font-size: 14px; font-weight: 700; font-family: 'Poppins', sans-serif;">
-                            Due Date
-                        </p>
-                        <div class="return quick-access-circle"></div>
-                        <div class="separator-line"></div>
+                        <div class="status-label-container">
+                            <p class="status-label">Due Date</p>
+                            <div class="quick-access-circle return"></div>
+                        </div>
+
+                        <div class="return-student-container-list">
 
                         <!-- Return Entries -->
-                        <div class="student-container">
-                            <div class="two-column-layout">
-                                <p class="student-name">Marvin Cayosa</p>
-                                <p class="return-date">Nov. 21, 2024</p>
+                            <div class="student-container">
+                                <div class="two-column-layout">
+                                    <p class="student-name">Marvin Cayosa</p>
+                                    <p class="return-date">Nov. 21, 2024</p>
 
+                                </div>
                             </div>
-                        </div>
 
-                        <div class="student-container">
-                            <div class="two-column-layout">
-                                <p class="student-name">Vincent Vinas</p>
-                                <p class="return-date">Nov. 21, 2024</p>
+                            <div class="student-container">
+                                <div class="two-column-layout">
+                                    <p class="student-name">Vincent Vinas</p>
+                                    <p class="return-date">Nov. 21, 2024</p>
+                                </div>
                             </div>
-                        </div>
 
-                        <div class="student-container">
-                            <div class="two-column-layout">
-                                <p class="student-name">Clarizza Reyes</p>
-                                <p class="return-date">Nov. 21, 2024</p>
+                            <div class="student-container">
+                                <div class="two-column-layout">
+                                    <p class="student-name">Clarizza Reyes</p>
+                                    <p class="return-date">Nov. 21, 2024</p>
+                                </div>
                             </div>
-                        </div>
 
-                        <div class="student-container">
-                            <div class="two-column-layout">
-                                <p class="student-name">Bernadette Virtucio</p>
-                                <p class="return-date">Nov. 21, 2024</p>
+                            <div class="student-container">
+                                <div class="two-column-layout">
+                                    <p class="student-name">Bernadette Virtucio</p>
+                                    <p class="return-date">Nov. 21, 2024</p>
+                                </div>
                             </div>
-                        </div>
 
-                        <!-- See More Link -->
-                        <div class="see-more-container">
-                            <a href="#" class="see-more-link" onclick="toggleRequestStatus()">
-                                See more
-                            </a>
-                        </div>
+                            <!-- See More Link -->
+                            <div class="see-more-container">
+                                <a href="#" class="see-more-link" onclick="toggleRequestStatus()">
+                                    See more
+                                </a>
+                            </div>
+                        </div>    
                     </div>
 
                 </div>
@@ -823,27 +730,6 @@
 </div>
 
 <script>
-    const toggleButton = document.getElementById("toggleSidebar");
-    const sidebar = document.querySelector(".sidebar_custom");
-    const iconNames = document.querySelectorAll(".icon_name");
-    const toggleIcon = document.getElementById("sidebarToggleIcon"); // Get the icon element
-
-    toggleButton.addEventListener("click", () => {
-        sidebar.classList.toggle("expanded");
-
-        // Toggle icon names visibility
-        iconNames.forEach((name) => {
-            name.classList.toggle("visible"); // Toggle visibility
-        });
-
-        // Change the icon based on sidebar state
-        if (sidebar.classList.contains("expanded")) {
-            toggleIcon.setAttribute("xlink:href", "icons.svg#icon-left"); // Change to icon-left
-        } else {
-            toggleIcon.setAttribute("xlink:href", "icons.svg#icon-right"); // Change to icon-right
-        }
-    });
-
     // Status Section Hidden Feature
     function toggleStatusSection() {
         const statusSection = document.querySelector(".status-section");
